527ae2900f917980ae436e38c837a142b90c231e
Manikanta <prvp6501@gmail.com>
2025-02-15 16:11:28 +0530

    Added options to download as json

--- utils.js
+++ utils.js
@@ -15,6 +15,20 @@ document.getElementById("download-csv").addEventListener("click", function () {
   a.click();
 });
 
+// Download JSON
+document.getElementById("download-json").addEventListener("click", function () {
+  const expenses = JSON.parse(localStorage.getItem("expenses")) || [];
+  if (expenses.length === 0) return alert("No expenses to download.");
+
+  const blob = new Blob([JSON.stringify(expenses, null, 2)], {
+    type: "application/json",
+  });
+  const a = document.createElement("a");
+  a.href = URL.createObjectURL(blob);
+  a.download = "expenses.json";
+  a.click();
+});
+
 document.getElementById("clear-all").addEventListener("click", function () {
   if (confirm("Are you sure you want to clear all expenses?")) {
     localStorage.removeItem("expenses");
